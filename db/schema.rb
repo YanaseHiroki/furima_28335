6fa9bde937aa30e5a65d700004845ed5381833f7
Hiroki Yanase <60053865+YanaseHiroki@users.noreply.github.com>
2020-08-11 14:08:10 +0900

    gem rails_12factorの追加

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 2020_08_10_045055) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
     t.string "title", null: false
     t.string "image", null: false
     t.text "description", null: false
@@ -26,26 +25,6 @@ ActiveRecord::Schema.define(version: 2020_08_10_045055) do
     t.integer "star", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", default: "", null: false
-    t.string "email", default: "", null: false
-    t.string "password", default: "", null: false
-    t.string "name_1", default: "", null: false
-    t.string "name_2", default: "", null: false
-    t.string "kana_1", default: "", null: false
-    t.string "kana_2", default: "", null: false
-    t.date "birthday", default: "0001-01-01", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  add_foreign_key "items", "users"
 end
