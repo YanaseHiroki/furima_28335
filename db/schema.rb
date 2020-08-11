ac2e908ed281caf3f552df027983fe8860454fe2
Hiroki Yanase <60053865+YanaseHiroki@users.noreply.github.com>
2020-08-11 18:12:46 +0900

    usersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_045055) do
+ActiveRecord::Schema.define(version: 2020_08_11_085433) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.string "title", null: false
     t.string "image", null: false
     t.text "description", null: false
@@ -25,6 +26,26 @@ ActiveRecord::Schema.define(version: 2020_08_10_045055) do
     t.integer "star", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.string "name_1", null: false
+    t.string "name_2", null: false
+    t.string "kana_1", null: false
+    t.string "kana_2", null: false
+    t.date "birthday", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "items", "users"
 end
