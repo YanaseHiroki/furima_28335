7114f27a6501def85d01849ce8a181a5b4e6b9e7
Hiroki Yanase <60053865+YanaseHiroki@users.noreply.github.com>
2020-08-15 18:40:56 +0900

    不要なコメントアウトの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    # params[:user][:birthday] = birthday_join
     @user = User.new(user_params)
     if @user.save
       redirect_to root_path
