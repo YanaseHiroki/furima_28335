24dabbbba6e117c430547dc97ee77541f1537f0c
Hiroki Yanase <60053865+YanaseHiroki@users.noreply.github.com>
2020-08-15 17:54:06 +0900

    Userモデルのバリデーションをリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  # 指定された属性が「空でない」ことを確認
-  validates :email, :password,  :nickname,  :name_1,  :name_2,  :kana_1,  :kana_2,  :birthday, presence: true
-  # 一意性と「@があるか」を確認
-  validates :email, format: { with: /@.+/ }
-  validates :email, uniqueness: true
-  # ６文字以上か確認
-  validates :password, length: { minimum: 6 }
-  # 文字の種類（ひらがな、カタカナ、漢字）の確認
-  validates :name_1, :name_2,   format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :kana_1, :kana_2,   format: { with: /\A[ァ-ン]+\z/ }
+
+  # すべての属性が「空でない」ことを確認
+  with_options presence: true do
+    validates :nickname, :birthday
+    # 一意性と「@があるか」を確認
+    validates :email, format: { with: /@.+/ }, uniqueness: true
+    # パスワードが英数字混合で構成されているか確認
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze }
+    # 文字の種類（ひらがな、カタカナ、漢字）の確認
+    validates :name_1, :name_2, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :kana_1, :kana_2, format: { with: /\A[ァ-ン]+\z/ }
+  end
 
   has_many :items
   # has_many :buys
