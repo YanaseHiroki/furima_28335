346e125b36d674f0cbe3ca8bad9ef6db3401406a
Hiroki Yanase <60053865+YanaseHiroki@users.noreply.github.com>
2020-08-15 17:52:58 +0900

    Rubocopの処理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :name_1, :name_2, :kana_1, :kana_2, :birthday])
   end
+
   private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,15 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
   end
-  
+
   def create
     # params[:user][:birthday] = birthday_join
-    @user = User.new(user_params) 
-
+    @user = User.new(user_params)
     if @user.save
       redirect_to root_path
     else
       render :new
     end
   end
-
-  # private
-
-  # def birthday_join
-  #   date = params[:user][:birthday]
-  #   require 'date'
-  #   Date.new date["birthday(1i)"].to_i,date["birthday(2i)"].to_i,date["birthday(3i)"].to_i
-  # end
-
-  # def user_params
-  #   params.require(:user).permit(
-  #     :email,
-  #     :password,
-  #     :nickname,
-  #     :name_1,
-  #     :name_2,
-  #     :kana_1,
-  #     :kana_2,
-  #     :birthday
-  #   )
-  # end
-
 end
