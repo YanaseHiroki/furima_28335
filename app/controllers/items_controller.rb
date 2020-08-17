7e9f559943d1175d33fab39727502ac23167fdff
Hiroki Yanase <60053865+YanaseHiroki@users.noreply.github.com>
2020-08-17 18:23:55 +0900

    ActiveStorageの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,34 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all.order(id: 'DESC')
   end
+  def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :title,
+      :image,
+      :description,
+      :price,
+      :category_id,
+      :state_id,
+      :charge_id,
+      :pref_id,
+      :due_id
+    ).merge(
+      star:'0',
+      user_id:current_user.id
+    )
+  end
 end
