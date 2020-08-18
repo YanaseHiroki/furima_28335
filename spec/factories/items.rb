6e26f1135dc1756b9f4537db5eb82e0f950cc03e
Hiroki Yanase <60053865+YanaseHiroki@users.noreply.github.com>
2020-08-18 16:40:02 +0900

    商品のモデル単体テストコードを作成

---
+++ spec/factories/items.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :item do
+    title       { Faker::Lorem.sentence }
+    description { Faker::Lorem.sentence }
+    category_id { Faker::Number.between(from: 2,to: 11) }
+    state_id    { Faker::Number.between(from: 2,to: 7) }
+    charge_id   { Faker::Number.between(from: 2,to: 3) }
+    pref_id     { Faker::Number.between(from: 2,to: 48) }
+    due_id      { Faker::Number.between(from: 2,to: 4) }
+    price       { Faker::Number.between(from: 300,to: 9999999) }
+    association :user
+  end
+end
