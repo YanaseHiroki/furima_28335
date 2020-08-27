d8aa838adbc9267d18f11bc4cc00a4dcbbfe095c
Hiroki Yanase <60053865+YanaseHiroki@users.noreply.github.com>
2020-08-27 17:37:07 +0900

    PAY.JPの決済昨日の導入

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,32 +1,68 @@
 class BuysController < ApplicationController
   before_action :authenticate_user!
-  before_action :buyer
+  before_action :set_item
+  # # before_action :buyer
 
-  def new
+  def index
     @buy = Buy.new
+  # @buy = BuyShip.new は Name Errorになる
   end
 
   def create
-    @buy = Buy.new(buy_params)
-    if @buy.save
-      redirect_to root_path
+    @buy = Buy.new(
+      user_id: buy_params[:user],
+      item_id: buy_params[:item]
+    )
+    @ship = Ship.new(
+      postal:  buy_params[:postal],
+      pref_id: buy_params[:pref_id],
+      city:    buy_params[:city],
+      number:  buy_params[:number],
+      house:   buy_params[:house]
+    )
+    if @buy.valid? && @ship.valid?
+      pay_item
+      @buy.save
+      @ship.save
+      return redirect_to root_path
     else
-      render :new
+      render 'index'
     end
   end
-
+  
   private
-
+  
+    def set_item
+      @item = Item.find(params[:item_id])
+    end
+  
   def buyer
-    if @buy.item.user_id == current_user.id
+    if @item.user_id == current_user.id
       redirect_to root_path
     end
   end
   
   def buy_params
     params.require(:buy).permit(
-      :user,
-      :item
+      :token,
+      :postal,
+      :pref_id,
+      :city,
+      :number,
+      :house,
+      :tel
+        ).merge(
+      user: @item.user_id,
+      item: @item.id
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      carrency: 'jpy'
     )
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,6 +1,21 @@
 class Ship < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :pref
-end
 
-# アソシエーション
-belongs_to :buy
+  # すべての属性が空でないことを確認
+  with_options presence: true do
+    validates :buy
+    # 7桁の郵便番号（ハイフンあり）か確認
+    validates :postal, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :pref_id
+    validates :city
+    validates :number
+    validates :house
+    # 11桁以下の数字か確認
+    validates :tel, format: { with: /\A\d{,11}\z/ }
+  end
+
+
+  # アソシエーション
+  belongs_to :buy
+end
